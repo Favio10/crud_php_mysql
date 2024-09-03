6744a9e4f3104cbae1c50f07eb3170a4e7f63726
Favio10 <favio_olivera@hotmail.com>
2024-09-02 23:53:46 -0300

    2 commit

---
+++ update.php
@@ -0,0 +1,34 @@
+<?php 
+include('connection.php');
+$con = connection();
+
+$id=$_GET['id'];
+
+$sql = "SELECT * FROM users WHERE id='$id'";
+$query = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($query)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar usuarios</title>
+</head>
+<body>
+    <div class="users-form">
+        <form action="edit_user.php" method="POST">
+            <h1>Editar usuario</h1>
+            <input type="hidden" name="id" value="<?= $row['id'] ?>">
+            <input type="text" name="name" placeholder="Name" value="<?= $row['name'] ?>">
+            <input type="text" name="lastname" placeholder="Lastname" value="<?= $row['lastname'] ?>">
+            <input type="text" name="username" placeholder="Username" value="<?= $row['username'] ?>">
+            <input type="text" name="password" placeholder="Password" value="<?= $row['password'] ?>">
+            <input type="text" name="email" placeholder="Email" value="<?= $row['email'] ?>">
+            
+            <input type="submit" value="Actualizar información">
+        </form>
+    </div>
+</body>
+</html>
